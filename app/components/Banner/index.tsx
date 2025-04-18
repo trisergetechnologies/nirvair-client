4a288fa4531fe7c25d34161e5085db2693cbe708
Prabhat Jha <prabhatjha940@gmail.com>
2025-04-19 00:30:16 +0530

    tagline change on banner

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
                     <div className='col-span-7 flex flex-col justify-evenly relative'>
                         <Image src="/assets/banner/star.svg" alt="star-image" width={95} height={97} className='absolute top-[-74px] right-[51px]' />
                         <Image src="/assets/banner/lineone.svg" alt="line-image" width={190} height={148} className='absolute top-[-74px] right-[51px]' />
-                        <h1 className='text-midnightblue text-4xl md:text-85xl text-center lg:text-start font-semibold lh-133 pt-5'>Deutsche Ingenieurskunst trifft digitale Innovation</h1>
+                        <h1 className='text-midnightblue text-2xl md:text-85xl text-center lg:text-start font-semibold lh-133 pt-5'>Wertegeleitet, technologiebasiert, wirkungsorientiert.</h1>
                         <h3 className='text-black opacity-75 text-lg font-normal text-center lg:text-start pt-8'>Nirvair entwickelt zukunftssichere digitale Lösungen mit Präzision, Qualität und modernster Technologie - deutsche Ingenieurexzellenz für Ihre digitale Transformation.</h3>
                         <div className='pt-8 mx-auto lg:mx-0'>
                             <button className="text-white text-xl font-medium py-6 px-12 rounded-full transition duration-150 ease-in-out bg-electricblue hover:text-white hover:bg-blue">
