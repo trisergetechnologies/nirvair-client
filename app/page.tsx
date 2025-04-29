ccfa8871d015e716162e372c6a6c06d7322df73d
Vaibhav Pandey <vaibhavkpandeys@gmail.com>
2025-04-29 22:58:32 +0530

    his

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import Banner from './components/Banner/index';
+import dynamic from 'next/dynamic';
+// import Banner from './components/Banner/index';
 import Business from './components/Business/index';
 import Features from './components/Features/index';
 import Logistic from './components/Logistic/index';
 import Payment from './components/Payment/index';
 import People from './components/People/index';
+
+const Banner = dynamic(()=>import('./components/Banner'))
 
 export default function Home() {
 
